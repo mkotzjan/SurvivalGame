77de93ef50e302854d95beedc53736d789f0d91c
ChaosBelcebub <ChaosBelcebub@users.noreply.github.com>
2015-01-02 17:10:59 +0100

    add Framework.Grafics

--- SurvivalGame/SurvivalGame/Enemy.cs
+++ SurvivalGame/SurvivalGame/Enemy.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace SurvivalGame
 {
