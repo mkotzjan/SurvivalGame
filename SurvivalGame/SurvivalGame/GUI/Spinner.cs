e16f65ba39c48678f1498751f52260be8bc7dc30
ChaosBelcebub <ChaosBelcebub@users.noreply.github.com>
2015-01-12 13:48:15 +0100

    Add two constructors

--- SurvivalGame/SurvivalGame/GUI/Spinner.cs
+++ SurvivalGame/SurvivalGame/GUI/Spinner.cs
@@ -7,5 +7,26 @@ namespace SurvivalGame.GUI
 {
     class Spinner
     {
+        // Variables
+        List<string> contentList;
+        Dictionary<int, string> contentDict;
+
+        /// <summary>
+        /// Constructor for Lists
+        /// </summary>
+        /// <param name="list"></param>
+        public Spinner(List<string> list)
+        {
+
+        }
+
+        /// <summary>
+        /// Constructor for Dictionarys
+        /// </summary>
+        /// <param name="dict"></param>
+        public Spinner(Dictionary<int, string> dict)
+        {
+
+        }
     }
 }
