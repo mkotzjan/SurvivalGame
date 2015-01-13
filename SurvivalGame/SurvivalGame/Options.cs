3d832de5e52fb719bfabc05feb85f6fecf5a9fcc
ChaosBelcebub <ChaosBelcebub@users.noreply.github.com>
2015-01-13 17:19:27 +0100

    add Options class

---
+++ SurvivalGame/SurvivalGame/Options.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace SurvivalGame
+{
+    class Options
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Options()
+        {
+
+        }
+
+        /// <summary>
+        /// Load content for options
+        /// </summary>
+        /// <param name="content"></param>
+        public void LoadContent(ContentManager content)
+        {
+
+        }
+
+        /// <summary>
+        /// Update options
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        /// <summary>
+        /// Draw options
+        /// </summary>
+        /// <param name="spriteBatch"></param>
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
