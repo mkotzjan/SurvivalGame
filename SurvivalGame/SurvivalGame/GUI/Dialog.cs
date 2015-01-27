c6141b8164a463a0d1c8fbbac373ebb330eb6d9d
ChaosBelcebub <ChaosBelcebub@users.noreply.github.com>
2015-01-27 13:26:00 +0100

    add Dialog class
    Added Dialog class with first implementation of Constructor

---
+++ SurvivalGame/SurvivalGame/GUI/Dialog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SurvivalGame.GUI
+{
+    class Dialog
+    {
+        string displayedText;
+        bool displayOkButton;
+
+        public Dialog(string text, bool okButton = true)
+        {
+            displayedText = text;
+            displayOkButton = okButton;
+        }
+
+        public void Draw()
+        {
+
+        }
+    }
+}
