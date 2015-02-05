97e376ad2ea1e9fac1a0379bb41ff77f46d3964f
ChaosBelcebub <ChaosBelcebub@users.noreply.github.com>
2015-02-05 17:21:26 +0100

    add second constructor for random seeds

--- SurvivalGame/SurvivalGame/MapMaker.cs
+++ SurvivalGame/SurvivalGame/MapMaker.cs
@@ -11,14 +11,19 @@ namespace SurvivalGame
     {
         private Texture2D mouseMap;
         private Texture2D slopeMaps;
+        private int seed;
         public List<MapRow> Rows = new List<MapRow>();
         public int MapWidth = 50;
         public int MapHeight = 50;
+
+        Random rnd;
 
         public MapMaker(Texture2D mouseMap, Texture2D slopeMap)
         {
             this.mouseMap = mouseMap;
             this.slopeMaps = slopeMap;
+            seed = (int)DateTime.Now.Ticks;
+            rnd = new Random(seed);
             for (int y = 0; y < MapHeight; y++)
             {
                 MapRow thisRow = new MapRow();
@@ -29,94 +34,111 @@ namespace SurvivalGame
                 Rows.Add(thisRow);
             }
             // Create Sample Map Data
-            Rows[0].Columns[3].TileID = 3;
-            Rows[0].Columns[4].TileID = 3;
-            Rows[0].Columns[5].TileID = 1;
-            Rows[0].Columns[6].TileID = 1;
-            Rows[0].Columns[7].TileID = 1;
+            //Rows[0].Columns[3].TileID = 3;
+            //Rows[0].Columns[4].TileID = 3;
+            //Rows[0].Columns[5].TileID = 1;
+            //Rows[0].Columns[6].TileID = 1;
+            //Rows[0].Columns[7].TileID = 1;
 
-            Rows[1].Columns[3].TileID = 3;
-            Rows[1].Columns[4].TileID = 1;
-            Rows[1].Columns[5].TileID = 1;
-            Rows[1].Columns[6].TileID = 1;
-            Rows[1].Columns[7].TileID = 1;
+            //Rows[1].Columns[3].TileID = 3;
+            //Rows[1].Columns[4].TileID = 1;
+            //Rows[1].Columns[5].TileID = 1;
+            //Rows[1].Columns[6].TileID = 1;
+            //Rows[1].Columns[7].TileID = 1;
 
-            Rows[2].Columns[2].TileID = 3;
-            Rows[2].Columns[3].TileID = 1;
-            Rows[2].Columns[4].TileID = 1;
-            Rows[2].Columns[5].TileID = 1;
-            Rows[2].Columns[6].TileID = 1;
-            Rows[2].Columns[7].TileID = 1;
+            //Rows[2].Columns[2].TileID = 3;
+            //Rows[2].Columns[3].TileID = 1;
+            //Rows[2].Columns[4].TileID = 1;
+            //Rows[2].Columns[5].TileID = 1;
+            //Rows[2].Columns[6].TileID = 1;
+            //Rows[2].Columns[7].TileID = 1;
 
-            Rows[3].Columns[2].TileID = 3;
-            Rows[3].Columns[3].TileID = 1;
-            Rows[3].Columns[4].TileID = 1;
-            Rows[3].Columns[5].TileID = 2;
-            Rows[3].Columns[6].TileID = 2;
-            Rows[3].Columns[7].TileID = 2;
+            //Rows[3].Columns[2].TileID = 3;
+            //Rows[3].Columns[3].TileID = 1;
+            //Rows[3].Columns[4].TileID = 1;
+            //Rows[3].Columns[5].TileID = 2;
+            //Rows[3].Columns[6].TileID = 2;
+            //Rows[3].Columns[7].TileID = 2;
 
-            Rows[4].Columns[2].TileID = 3;
-            Rows[4].Columns[3].TileID = 1;
-            Rows[4].Columns[4].TileID = 1;
-            Rows[4].Columns[5].TileID = 2;
-            Rows[4].Columns[6].TileID = 2;
-            Rows[4].Columns[7].TileID = 2;
+            //Rows[4].Columns[2].TileID = 3;
+            //Rows[4].Columns[3].TileID = 1;
+            //Rows[4].Columns[4].TileID = 1;
+            //Rows[4].Columns[5].TileID = 2;
+            //Rows[4].Columns[6].TileID = 2;
+            //Rows[4].Columns[7].TileID = 2;
 
-            Rows[5].Columns[2].TileID = 3;
-            Rows[5].Columns[3].TileID = 1;
-            Rows[5].Columns[4].TileID = 1;
-            Rows[5].Columns[5].TileID = 2;
-            Rows[5].Columns[6].TileID = 2;
-            Rows[5].Columns[7].TileID = 2;
-            Rows[16].Columns[4].AddHeightTile(54);
+            //Rows[5].Columns[2].TileID = 3;
+            //Rows[5].Columns[3].TileID = 1;
+            //Rows[5].Columns[4].TileID = 1;
+            //Rows[5].Columns[5].TileID = 2;
+            //Rows[5].Columns[6].TileID = 2;
+            //Rows[5].Columns[7].TileID = 2;
+            //Rows[16].Columns[4].AddHeightTile(54);
 
-            Rows[17].Columns[3].AddHeightTile(54);
+            //Rows[17].Columns[3].AddHeightTile(54);
 
-            Rows[15].Columns[3].AddHeightTile(54);
-            Rows[16].Columns[3].AddHeightTile(53);
+            //Rows[15].Columns[3].AddHeightTile(54);
+            //Rows[16].Columns[3].AddHeightTile(53);
 
-            Rows[15].Columns[4].AddHeightTile(54);
-            Rows[15].Columns[4].AddHeightTile(54);
-            Rows[15].Columns[4].AddHeightTile(51);
+            //Rows[15].Columns[4].AddHeightTile(54);
+            //Rows[15].Columns[4].AddHeightTile(54);
+            //Rows[15].Columns[4].AddHeightTile(51);
 
-            Rows[18].Columns[3].AddHeightTile(51);
-            Rows[19].Columns[3].AddHeightTile(50);
-            Rows[18].Columns[4].AddHeightTile(55);
+            //Rows[18].Columns[3].AddHeightTile(51);
+            //Rows[19].Columns[3].AddHeightTile(50);
+            //Rows[18].Columns[4].AddHeightTile(55);
 
-            Rows[14].Columns[4].AddHeightTile(54);
+            //Rows[14].Columns[4].AddHeightTile(54);
 
-            Rows[14].Columns[5].AddHeightTile(62);
-            Rows[14].Columns[5].AddHeightTile(61);
-            Rows[14].Columns[5].AddHeightTile(63);
+            //Rows[14].Columns[5].AddHeightTile(62);
+            //Rows[14].Columns[5].AddHeightTile(61);
+            //Rows[14].Columns[5].AddHeightTile(63);
 
-            Rows[17].Columns[4].AddTopperTile(114);
-            Rows[16].Columns[5].AddTopperTile(115);
-            Rows[14].Columns[4].AddTopperTile(125);
-            Rows[15].Columns[5].AddTopperTile(91);
-            Rows[16].Columns[6].AddTopperTile(94);
+            //Rows[17].Columns[4].AddTopperTile(114);
+            //Rows[16].Columns[5].AddTopperTile(115);
+            //Rows[14].Columns[4].AddTopperTile(125);
+            //Rows[15].Columns[5].AddTopperTile(91);
+            //Rows[16].Columns[6].AddTopperTile(94);
 
-            Rows[15].Columns[5].Walkable = false;
-            Rows[16].Columns[6].Walkable = false;
+            //Rows[15].Columns[5].Walkable = false;
+            //Rows[16].Columns[6].Walkable = false;
 
-            Rows[22].Columns[9].AddHeightTile(34);
-            Rows[21].Columns[9].AddHeightTile(34);
-            Rows[21].Columns[8].AddHeightTile(34);
-            Rows[20].Columns[9].AddHeightTile(34);
+            //Rows[22].Columns[9].AddHeightTile(34);
+            //Rows[21].Columns[9].AddHeightTile(34);
+            //Rows[21].Columns[8].AddHeightTile(34);
+            //Rows[20].Columns[9].AddHeightTile(34);
 
-            Rows[22].Columns[8].AddTopperTile(31);
-            Rows[22].Columns[8].SlopeMap = 0;
-            Rows[23].Columns[8].AddTopperTile(31);
-            Rows[23].Columns[8].SlopeMap = 0;
+            //Rows[22].Columns[8].AddTopperTile(31);
+            //Rows[22].Columns[8].SlopeMap = 0;
+            //Rows[23].Columns[8].AddTopperTile(31);
+            //Rows[23].Columns[8].SlopeMap = 0;
 
-            Rows[22].Columns[10].AddTopperTile(32);
-            Rows[22].Columns[10].SlopeMap = 1;
-            Rows[23].Columns[9].AddTopperTile(32);
-            Rows[23].Columns[9].SlopeMap = 1;
+            //Rows[22].Columns[10].AddTopperTile(32);
+            //Rows[22].Columns[10].SlopeMap = 1;
+            //Rows[23].Columns[9].AddTopperTile(32);
+            //Rows[23].Columns[9].SlopeMap = 1;
 
-            Rows[24].Columns[9].AddTopperTile(30);
-            Rows[24].Columns[9].SlopeMap = 4;
+            //Rows[24].Columns[9].AddTopperTile(30);
+            //Rows[24].Columns[9].SlopeMap = 4;
 
             // End Create Sample Map Data
+        }
+
+        public MapMaker(Texture2D mouseMap, Texture2D slopeMap, int seed)
+        {
+            this.mouseMap = mouseMap;
+            this.slopeMaps = slopeMap;
+            seed = (int)DateTime.Now.Ticks;
+            rnd = new Random(seed);
+            for (int y = 0; y < MapHeight; y++)
+            {
+                MapRow thisRow = new MapRow();
+                for (int x = 0; x < MapWidth; x++)
+                {
+                    thisRow.Columns.Add(new MapCell(0));
+                }
+                Rows.Add(thisRow);
+            }
         }
 
         public Point WorldToMapCell(Point worldPoint, out Point localPoint)
