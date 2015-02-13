1a6aade8207068dfac0a80177d807affac296687
ChaosBelcebub <ChaosBelcebub@users.noreply.github.com>
2015-02-13 09:49:16 +0100

    first implementation of an xml writer
    doesn't compile

--- SurvivalGame/SurvivalGame/Saving.cs
+++ SurvivalGame/SurvivalGame/Saving.cs
@@ -13,6 +13,15 @@
 
         private static void SaveCurrentGame()
         {
+            MapMaker myMap = new MapMaker();
+            System.Xml.Serialization.XmlSerializer writer =
+                new System.Xml.Serialization.XmlSerializer(typeof(myMap));
+
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "//SerializationOverview.xml";
+            System.IO.FileStream file = System.IO.File.Create(path);
+
+            writer.Serialize(file, overview);
+            file.Close();
         }
     }
 }
